70d86103d652438a8c1b0814bea77428a0723889
Sasha Klein <sashafklein@gmail.com>
2014-07-07 23:24:10 -0700

    Louder arrival

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,7 +3,7 @@ class QuotesController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:create]
   
   def create
-    puts "INCOMING PARAMS HERE: #{params}"
+    raise StandardError.new(params)
     head 200
   end
 end
