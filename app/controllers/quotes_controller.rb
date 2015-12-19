90feaebbda1656e5ce24fd8c909c4c49f2508439
Sasha Klein <sashafklein@gmail.com>
2015-12-19 14:00:18 -0800

    trim => strip

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -16,10 +16,10 @@ class QuotesController < ApplicationController
       puts "PARAMS: #{ params.to_s }"
       if subject.include?("DELETE")
         puts "IN DELETE"
-        Quote.delete!(user, subject.split("DELETE:")[1].trim, body)
+        Quote.delete!(user, subject.split("DELETE:")[1].strip, body)
       elsif subject.include?("EDIT")
         puts "IN EDIT"
-        Quote.edit!(user, subject.split("EDIT:")[1].trim, body)
+        Quote.edit!(user, subject.split("EDIT:")[1].strip, body)
       else 
         puts "HERE. SUBJECT: #{subject}"
         user.add_quote({author: subject, body: body})
