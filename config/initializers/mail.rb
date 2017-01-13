806cf46c7a18f06fefe785580017914092421b49
Sasha Klein <sashafklein@gmail.com>
2017-01-13 09:57:26 -0800

    Fixed?

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  port:              587, 
+  port:              587,
   address:           'smtp.mailgun.org',
   user_name:         ENV['MAILGUN_SMTP_LOGIN'],
   password:          ENV['MAILGUN_SMTP_PASSWORD'],
-  domain:            'appurlhere.com',
+  domain:            'https://owl-app.herokuapp.com/',
   authentication:    :plain,
   content_type:      'text/html'
 }
